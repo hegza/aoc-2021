06031bc94636c49d1761c8221c232bce623e6c31
Henri Lunnikivi <heggggxa@gmail.com>
2021-12-10 08:16:17 +0200

    Refactor until it felt good

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,142 +1,82 @@
-use anyhow::Context;
-use itertools::{self, Itertools};
-use maplit::hashmap;
-use std::collections::{HashMap, VecDeque};
+use std::collections::VecDeque;
+
+use itertools::Itertools;
+
+fn value(c: char) -> u64 {
+    match c {
+        '(' => 1,
+        '[' => 2,
+        '{' => 3,
+        '<' => 4,
+        _ => panic!(),
+    }
+}
 
 fn main() -> anyhow::Result<()> {
     let lines = include_str!("input.txt").lines();
-    let incomplete = lines.enumerate().filter_map(|(idx, l)| {
-        let res = parse(l);
-        /*
-        match &res {
-            Ok(_) => {}
-            Err(e) => println!("Parse error on line: {} at {}", idx + 1, e.loc + 1),
-        }
-        */
-        res.ok()
-    }); //.collect::<Vec<ParseError>>();
 
-    let mut line_scores = vec![];
+    let incomplete = lines.filter_map(|l| parse(l).ok());
 
-    for line in incomplete.map(|closes| closes.into_iter().rev()) {
-        let mut line_score: u64 = 0;
-        for c in line {
-            line_score *= 5;
-            line_score += match c {
-                '(' => 1,
-                '[' => 2,
-                '{' => 3,
-                '<' => 4,
-                _ => panic!(),
-            };
-        }
-        line_scores.push(line_score);
-    }
+    let mut line_scores = incomplete
+        .map(|missing| {
+            missing
+                .into_iter()
+                .rev()
+                .fold(0, |score, c| score * 5 + value(c))
+        })
+        .collect_vec();
+
     line_scores.sort();
 
     let mid_score = line_scores[line_scores.len() / 2];
+    println!("{}", mid_score);
 
-    /*
-    let score: u32 = incomplete
-        .map(|e| match e.kind {
-            ParenKind::Paren => 3,
-            ParenKind::Square => 57,
-            ParenKind::Brace => 1197,
-            ParenKind::Angle => 25137,
-            ParenKind::Unknown => panic!("{:?}", e.kind),
-        })
-        .sum();
-        */
-
-    println!("mid-score: {}", mid_score);
     Ok(())
 }
 
 struct ParseError {
-    kind: ParenKind,
+    kind: char,
     loc: usize,
 }
 
-#[derive(Debug)]
-enum ParenKind {
-    // '('
-    Paren,
-    // '['
-    Square,
-    // '{'
-    Brace,
-    // '<'
-    Angle,
-    Unknown,
+fn matching(c: char) -> char {
+    match c {
+        ')' => '(',
+        ']' => '[',
+        '}' => '{',
+        '>' => '<',
+        _ => panic!(),
+    }
+}
+
+struct ParseStack(VecDeque<char>);
+
+impl ParseStack {
+    fn push(&mut self, c: char) {
+        self.0.push_back(c);
+    }
+    fn pop(&mut self, c: char, loc: usize) -> Result<(), ParseError> {
+        if let Some(back) = self.0.pop_back() {
+            if back != matching(c) {
+                return Err(ParseError { kind: '(', loc });
+            }
+        }
+        Ok(())
+    }
 }
 
 fn parse(line: &str) -> Result<VecDeque<char>, ParseError> {
     let mut chars = line.chars().enumerate();
 
-    let mut open_stack = VecDeque::new();
+    let mut parse_stack = ParseStack(VecDeque::new());
 
     while let Some((loc, c)) = chars.next() {
         match c {
-            '(' => {
-                open_stack.push_back('(');
-            }
-            ')' => {
-                if let Some(back) = open_stack.pop_back() {
-                    if back != '(' {
-                        return Err(ParseError {
-                            kind: ParenKind::Paren,
-                            loc,
-                        });
-                    }
-                }
-            }
-            '[' => {
-                open_stack.push_back('[');
-            }
-            ']' => {
-                if let Some(back) = open_stack.pop_back() {
-                    if back != '[' {
-                        return Err(ParseError {
-                            kind: ParenKind::Square,
-                            loc,
-                        });
-                    }
-                }
-            }
-            '{' => {
-                open_stack.push_back('{');
-            }
-            '}' => {
-                if let Some(back) = open_stack.pop_back() {
-                    if back != '{' {
-                        return Err(ParseError {
-                            kind: ParenKind::Brace,
-                            loc,
-                        });
-                    }
-                }
-            }
-            '<' => {
-                open_stack.push_back('<');
-            }
-            '>' => {
-                if let Some(back) = open_stack.pop_back() {
-                    if back != '<' {
-                        return Err(ParseError {
-                            kind: ParenKind::Angle,
-                            loc,
-                        });
-                    }
-                }
-            }
-            _ => {
-                return Err(ParseError {
-                    kind: ParenKind::Unknown,
-                    loc,
-                })
-            }
+            '(' | '[' | '{' | '<' => parse_stack.push(c),
+            ')' | ']' | '}' | '>' => parse_stack.pop(c, loc)?,
+            _ => panic!("column {}", loc + 1),
         }
     }
 
-    Ok(open_stack)
+    Ok(parse_stack.0)
 }
