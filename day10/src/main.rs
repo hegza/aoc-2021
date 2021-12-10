4bce265a2b93b98db4e243aa33f870391cef8f0c
Henri Lunnikivi <heggggxa@gmail.com>
2021-12-10 07:57:40 +0200

    Day 10 part 2

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -5,16 +5,39 @@ use std::collections::{HashMap, VecDeque};
 
 fn main() -> anyhow::Result<()> {
     let lines = include_str!("input.txt").lines();
-    let errs = lines.enumerate().filter_map(|(idx, l)| {
+    let incomplete = lines.enumerate().filter_map(|(idx, l)| {
         let res = parse(l);
+        /*
         match &res {
             Ok(_) => {}
             Err(e) => println!("Parse error on line: {} at {}", idx + 1, e.loc + 1),
         }
-        res.err()
+        */
+        res.ok()
     }); //.collect::<Vec<ParseError>>();
 
-    let score: u32 = errs
+    let mut line_scores = vec![];
+
+    for line in incomplete.map(|closes| closes.into_iter().rev()) {
+        let mut line_score: u64 = 0;
+        for c in line {
+            line_score *= 5;
+            line_score += match c {
+                '(' => 1,
+                '[' => 2,
+                '{' => 3,
+                '<' => 4,
+                _ => panic!(),
+            };
+        }
+        line_scores.push(line_score);
+    }
+    line_scores.sort();
+
+    let mid_score = line_scores[line_scores.len() / 2];
+
+    /*
+    let score: u32 = incomplete
         .map(|e| match e.kind {
             ParenKind::Paren => 3,
             ParenKind::Square => 57,
@@ -23,8 +46,9 @@ fn main() -> anyhow::Result<()> {
             ParenKind::Unknown => panic!("{:?}", e.kind),
         })
         .sum();
+        */
 
-    println!("{}", score);
+    println!("mid-score: {}", mid_score);
     Ok(())
 }
 
@@ -46,7 +70,7 @@ enum ParenKind {
     Unknown,
 }
 
-fn parse(line: &str) -> Result<(), ParseError> {
+fn parse(line: &str) -> Result<VecDeque<char>, ParseError> {
     let mut chars = line.chars().enumerate();
 
     let mut open_stack = VecDeque::new();
@@ -114,5 +138,5 @@ fn parse(line: &str) -> Result<(), ParseError> {
         }
     }
 
-    Ok(())
+    Ok(open_stack)
 }
